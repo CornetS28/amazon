5e4973565f4683616eafb9e18e2c55b7a8a2eeee
CornetS28 <corsam28@gmail.com>
2021-01-10 22:38:33 -0500

    Connect the product for to its endpoint

--- admin/pages/product.vue
+++ admin/pages/product.vue
@@ -10,14 +10,14 @@
                <div class="a-spacing-top-medium" /> 
                <!-- Category Dropdown option -->
                <label> Category </label>
-               <select class="a-select-option">
+               <select class="a-select-option" v-model="categoryID">
                  <option v-for="category in categories" :value="category._id" :key="category._id"> {{ category.type }}</option>
                </select>
 
               <!-- Owner Dropdown option -->
               <div class="a-spacing-top-medium">
                 <label> Owner</label>
-                <select class="a-select-option">
+                <select class="a-select-option" v-model="ownerID">
                   <option v-for="owner in owners" 
                      :value="owner._id" 
                      :key="owner._id"> 
@@ -29,39 +29,44 @@
                 <!-- Title input -->
               <div class="a-spacing-top-medium">
                 <label style="margin-bottom: 0px">Title</label>
-                  <input type="text" class="a-input-text" style="width: 100%" />
+                  <input type="text" class="a-input-text" style="width: 100%" v-model="title"/>
               </div>
 
                <!--  Price input -->
               <div class="a-spacing-top-medium">
                 <label style="margin-bottom: 0px">Price</label>
-                  <input type="text" class="a-input-text" style="width: 100%" />
+                  <input type="text" class="a-input-text" style="width: 100%" v-model="price"/>
+              </div>
+
+               <!--  stock Quantity input -->
+              <div class="a-spacing-top-medium">
+                <label style="margin-bottom: 0px">Stock Quantity</label>
+                  <input type="text" class="a-input-text" style="width: 100%" v-model="stockQuantity"/>
               </div>
 
                <!--  Description input -->
               <div class="a-spacing-top-medium">
                 <label style="margin-bottom: 0px">Description</label>
-                  <textarea placeholder="Provide details such as product description" style="width: 100%" />
+                  <textarea placeholder="Provide details such as product description" style="width: 100%" v-model="description"/>
               </div>
 
               <!--  Photo file -->
               <div class="a-spacing-top-medium">
                 <label style="margin-bottom: 0px">Add Photo</label>
                   <div class="a-row a-spacing-top-medium">
-                    <label for="
-                    " class="choosefile-button">
-                    <i class="fas fa-plus"></i>
-                    <input type="file" />
-                    <p style="margin-top: -70px">name of the</p>
+                    <label class="choosefile-button">
+                      <i class="fas fa-plus"></i>
+                      <input type="file" @change="onFileSelected"/>
+                      <p style="margin-top: -70px">{{ fileName }}</p>
                     </label>
-                  </div>
+                  </div> 
               </div>
               <hr />
                <!--  Button -->
               <div class="a-spacing-top-large">
                 <span class="a-button-register">
                   <span class="span a-button-inner">
-                    <span class="a-button-text">App product</span>
+                    <span class="a-button-text" @click="onAddProduct">App product</span>
                   </span>
                 </span>
               </div>
@@ -93,6 +98,41 @@ export default {
     } catch (error) {
       console.log(error)
     }
+  },
+  data() {
+    return {
+      categoryID: null,
+      ownerID: null,
+      title: '',
+      price: 0,
+      description: '',
+      stockQuantity: 1,
+      selectedFile: null,
+      fileName: null,
+    }
+  },
+
+  methods: {
+    onFileSelected(event) {
+      this.selectedFile = event.target.files[0];
+      console.log(this.selectedFile);
+      this.fileName = event.target.files[0].name;
+    },
+    async onAddProduct() {
+      let data = new FormData();
+      data.append("title", this.title);
+      data.append("price", this.price);
+      data.append("description", this.description);
+      data.append("stockQuantity", this.stockQuantity);
+      data.append("ownerID", this.ownerID);
+      data.append("categoryID", this.categoryID);
+      data.append("photo", this.selectedFile, this.selectedFile.name);
+
+      let result = await this.$axios.$post("http://localhost:3000/api/products", data);
+
+      this.$router.push("/");
+    }
   }
 }
+
 </script>
